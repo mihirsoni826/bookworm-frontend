fbc29e0f0d695869341ad3879393441a6249668b
Mihir Soni <mihirsoni826@gmail.com>
2024-05-19 18:53:10 +0100

    Added functionality to navigate to home page from titlebar

--- src/components/Titlebar.tsx
+++ src/components/Titlebar.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Titlebar() {
+    const navigate = useNavigate();
+    function navigateToHome(): void {
+        navigate("/");
+    }
+
     return (
         <div className="bg-white my-auto py-4 px-6 items-center fixed inset-x-0 top-0 left-16 z-0">
-            <span className="text-lg font-bold">RAD</span>
-            <span className="text-lg font-bold text-[#8b211d]">ICAL</span>
+            <div className="w-fit cursor-pointer" onClick={navigateToHome}>
+                <span className="text-lg font-bold">RAD</span>
+                <span className="text-lg font-bold text-[#8b211d]">ICAL</span>
+            </div>
         </div>
     );
 }
