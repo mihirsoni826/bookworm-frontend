bcc049a07c58ee806985163191d0c752cc85ee9d
Mihir Soni <mihirsoni826@gmail.com>
2024-05-20 21:45:34 +0100

    Added navigation from sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import avatar from "../assets/avatar.jpeg";
 import Settings from "../assets/Settings.svg";
 import Stats from "../assets/Stats.svg";
 import LineHeart from "../assets/LineHeart.svg";
 
 export default function Sidebar() {
+    const navigate = useNavigate();
+
+    // Define active paths for each icon
+    const isActiveStats = ["/bestsellers", "/", "/home"].includes(
+        location.pathname
+    );
+    const isActiveFavourites =
+        ["/favourites", "/book"].includes(location.pathname) ||
+        location.pathname.startsWith("/book");
+
     return (
         <div className="h-screen w-16 bg-midnight-indigo text-white flex flex-col items-center inset-y-0 left-0 fixed z-10">
             <div className="flex bg-cool-mist w-full items-center justify-center p-3 pb-3">
@@ -14,35 +25,53 @@ export default function Sidebar() {
                 />
             </div>
             <div className="w-full flex flex-col flex-1 items-center justify-center bg-midnight-indigo pb-20">
-                <div className="cursor-pointer w-full flex flex-col items-center justify-center hover:bg-gradient-to-b from-b1 to-b2 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)]">
-                    <a href="#" className="pt-6 pb-2">
+                <div
+                    // className="cursor-pointer w-full flex flex-col items-center justify-center hover:w-20 hover:bg-gradient-to-b from-b1 to-b2 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)]"
+                    className={`cursor-pointer w-full flex flex-col items-center justify-center hover:w-20 hover:bg-gradient-to-b from-b1 to-b2 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)] ${
+                        isActiveStats
+                            ? "bg-gradient-to-b from-b1 to-b2 w-20 rounded-r-lg shadow-[0px_5px_15px_0px_rgba(61,76,108,1)]"
+                            : ""
+                    }`}
+                    onClick={() => navigate("/")}
+                >
+                    <div className="pt-6 pb-2">
                         <img
                             src={Stats}
                             alt="Statistics icon"
                             className="h-6 w-6"
                         />
-                    </a>
+                    </div>
                     <div className="w-1/4 border-b pt-4" />
                 </div>
                 <hr />
-                <div className="cursor-pointer w-full flex flex-col items-center justify-center hover:bg-gradient-to-b from-b1 to-b2 hover:w-22 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)]">
-                    <a href="#" className="pt-6 pb-2">
+                <div
+                    className={`cursor-pointer w-full flex flex-col items-center justify-center hover:w-20 hover:bg-gradient-to-b from-b1 to-b2 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)] ${
+                        isActiveFavourites
+                            ? "bg-gradient-to-b from-b1 to-b2 w-20 rounded-r-lg shadow-[0px_5px_15px_0px_rgba(61,76,108,1)]"
+                            : ""
+                    }`}
+                    onClick={() => navigate("/favourites")}
+                >
+                    <div className="pt-6 pb-2">
                         <img
                             src={LineHeart}
                             alt="Favourites heart icon"
                             className="h-6 w-6"
                         />
-                    </a>
+                    </div>
                     <div className="w-1/4 border-b pt-4" />
                 </div>
-                <div className="cursor-pointer w-full flex flex-col items-center justify-center hover:bg-gradient-to-b from-b1 to-b2 hover:w-22 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)]">
-                    <a href="#" className="pt-6 pb-2">
+                <div
+                    className="cursor-pointer w-full flex flex-col items-center justify-center hover:w-20 hover:bg-gradient-to-b from-b1 to-b2 hover:w-22 hover:rounded-r-lg hover:shadow-[0px_5px_15px_0px_rgba(61, 76, 108, 1)]"
+                    onClick={() => navigate("/")}
+                >
+                    <div className="pt-6 pb-2">
                         <img
                             src={Settings}
                             alt="Settings icon"
                             className="h-6 w-6"
                         />
-                    </a>
+                    </div>
                     <div className="w-1/4 border-b pt-4" />
                 </div>
             </div>
