db32323404f3fcf10d6e63f1768329e5f61229da
Mihir Soni <mihirsoni826@gmail.com>
2024-05-21 00:15:01 +0100

    Made book list card component responsive

--- src/components/BookListCard.tsx
+++ src/components/BookListCard.tsx
@@ -10,13 +10,7 @@ import {
 } from "../services/BookwormService";
 import { useNavigate } from "react-router-dom";
 
-const BookListCard = ({
-    book,
-    onFavoriteStatusChange,
-}: {
-    book: Book;
-    onFavoriteStatusChange: any;
-}) => {
+const BookListCard = ({ book }: { book: Book }) => {
     const [currBook, setCurrBook] = React.useState(book);
     const [isFavourite, setIsFavourite] = React.useState(currBook.favourite);
     const navigate = useNavigate();
@@ -29,7 +23,6 @@ const BookListCard = ({
         }
         setIsFavourite(!isFavourite);
         setCurrBook({ ...currBook, favourite: !isFavourite });
-        onFavoriteStatusChange({ ...currBook, favourite: !isFavourite });
     };
 
     function navigateToBookView(book: Book): void {
@@ -37,48 +30,53 @@ const BookListCard = ({
     }
 
     return (
-        <div key={currBook.isbn} className="flex flex-row bg-white py-3 px-5">
-            <div
-                className="content-center cursor-pointer"
-                onClick={() => navigateToBookView(currBook)}
-            >
-                <img
-                    src={BookIcon}
-                    alt="Book icon"
-                    className="w-6 h-6 mr-4 basis-0"
-                />
+        <div className="flex flex-col md:flex-row bg-white py-3 px-5">
+            <div className="basis-4/6 space-x-2 md:space-x-0 flex flex-row pr-0">
+                <div
+                    className="content-center cursor-pointer"
+                    onClick={() => navigateToBookView(currBook)}
+                >
+                    <img
+                        src={BookIcon}
+                        alt="Book icon"
+                        className="mr-4 basis-0"
+                    />
+                </div>
+                <p
+                    className="grow content-center cursor-pointer"
+                    onClick={() => navigateToBookView(currBook)}
+                >
+                    <span className="font-semibold">{currBook.title}</span> by{" "}
+                    {currBook.author}
+                </p>
             </div>
-            <p
-                className="basis-3/5 content-center cursor-pointer"
-                onClick={() => navigateToBookView(currBook)}
-            >
-                <span className="font-semibold">{currBook.title}</span> by{" "}
-                {currBook.author}
-            </p>
-            <div
-                className="basis-1/5 text-right content-center my-auto"
-                onClick={() => navigateToBookView(currBook)}
-            >
-                <Rating
-                    value={currBook.rating}
-                    unratedColor="gray"
-                    ratedColor="orange"
-                    readonly={true}
-                />
-            </div>
-            <p
-                className="basis-1/5 text-center content-center cursor-pointer"
-                onClick={() => navigateToBookView(currBook)}
-            >
-                {currBook.price} GBP
-            </p>
-            <div className="content-center">
-                <img
-                    src={isFavourite ? FilledHeart : LineHeart}
-                    alt="Favourites heart icon"
-                    className="w-6 h-6 mr-4 basis-0 cursor-pointer"
-                    onClick={handleFavourite}
-                />
+            <div className="flex mt-6 md:mt-0 md:flex md:flex-row basis-2/6 grow justify-between ">
+                <div
+                    className="md:content-center md:my-auto"
+                    onClick={() => navigateToBookView(currBook)}
+                >
+                    <Rating
+                        value={currBook.rating}
+                        unratedColor="gray"
+                        ratedColor="orange"
+                        readonly={true}
+                    />
+                </div>
+
+                <p
+                    className="md:text-center md:content-center md:cursor-pointer"
+                    onClick={() => navigateToBookView(currBook)}
+                >
+                    {currBook.price} GBP
+                </p>
+                <div className="content-center">
+                    <img
+                        src={isFavourite ? FilledHeart : LineHeart}
+                        alt="Favourites heart icon"
+                        className="md:cursor-pointer"
+                        onClick={handleFavourite}
+                    />
+                </div>
             </div>
         </div>
     );
